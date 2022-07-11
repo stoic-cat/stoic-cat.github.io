564c442e7e429d253f6da828bd6eff29a892317e
stoic-cat <109066175+stoic-cat@users.noreply.github.com>
2022-07-11 21:04:48 +0800

    add deplyoment branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
   organizationName: 'stoic-cat', // Usually your GitHub org/user name.
   projectName: 'stoic-cat.github.io', // Usually your repo name.
   trailingSlash: false,
+  deploymentBranch: "main",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
